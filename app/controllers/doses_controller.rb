5464d28504177253e27ccfae1e7751f249b00a3e
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-02-18 16:33:05 +0100

    rake is green, view and links are working - styling need to be done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,33 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
+  end
+
+  # {"dose"=>{"ingredient_id"=>"104"}, "commit"=>"Add new dose", "cocktail_id"=>"2"}
+
+  def create
+    # ingredient = Ingredient.find(params[:dose][:ingredient_id].to_i)
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
